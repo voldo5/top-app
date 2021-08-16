e60bfe107761d8a56d4fd9139a243ff687d04542
voldo5 <45669506+voldo5@users.noreply.github.com>
2021-08-16 22:16:10 +0300

    use svg svgr2

--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
 module.exports = {
-  reactStrictMode: true,
-}
+	webpack(config) {
+		config.module.rules.push({
+			test: /\.svg$/,
+			issuer: /\.(js|ts)x?$/,
+			use: ['@svgr/webpack'],
+		});
+
+		return config;
+	},
+};
+
+//issuer: { test: /\.(js|ts)x?$/,},
+//test: /\.(js|ts)x?$/,
+// webpack(config) {
