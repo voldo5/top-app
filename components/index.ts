04d671244d0f98532342e0f471a01c0ae2f51d0d
voldo5 <45669506+voldo5@users.noreply.github.com>
2021-08-18 20:27:02 +0300

    Component Tag

--- components/index.ts
+++ components/index.ts
@@ -1,6 +1,7 @@
 export * from './Htag/Htag';
 export * from './Button/Button';
 export * from './Paragraph/Paragraph';
+export * from './Tag/Tag';
 
 // export * from './Htag/Htag';
 // export * from './Button/Button';
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Htag, Paragraph } from "../components";
+import { Button, Htag, Paragraph, Tag } from "../components";
 
 export default function Home(): JSX.Element {
   return (
@@ -14,6 +14,14 @@ export default function Home(): JSX.Element {
       <Paragraph fontSize="l">Большой</Paragraph>
       <Paragraph>Средний</Paragraph>
       <Paragraph fontSize="s">Маленький</Paragraph>
+      <Tag size="s">Ghost</Tag>
+      <Tag size="m" color="red">
+        Red
+      </Tag>
+      <Tag size="s" color="green">
+        Green
+      </Tag>
+      <Tag color="primary">Green</Tag>
     </>
   );
 }
