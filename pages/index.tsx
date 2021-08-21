33854af58791707305135879d187d7537091566b
voldo5 <45669506+voldo5@users.noreply.github.com>
2021-08-21 20:53:35 +0300

    09 HOC компоненты

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import { Button, Htag, Paragraph, Rating, Tag } from "../components";
+import { withLayout } from "../layout/Layout";
 
-export default function Home(): JSX.Element {
+function Home(): JSX.Element {
   const [rating, setRating] = useState<number>(4);
 
   return (
@@ -24,8 +25,9 @@ export default function Home(): JSX.Element {
         Green
       </Tag>
       <Tag color="primary">Green</Tag>
-      <Rating rating={4} />
       <Rating rating={rating} isEditable setRating={setRating} />
     </>
   );
 }
+
+export default withLayout(Home);
