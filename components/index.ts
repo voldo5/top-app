7880bb40654bce0a2f8ba545991d6c16826db3f3
voldo5 <45669506+voldo5@users.noreply.github.com>
2021-09-08 14:53:44 +0300

    004 Компонент продукта - планирование

--- components/index.ts
+++ components/index.ts
@@ -8,6 +8,7 @@ export * from './Sort/Sort';
 export * from './Input/Input'; 
 export * from './Textarea/Textarea';
 export * from './Search/Search';
+export * from './Product/Product';
 
 // export * from './Htag/Htag';
 // export * from './Button/Button';
--- page-components/TopPageComponent/TopPageComponent.tsx
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -1,4 +1,11 @@
-import { Advantages, Htag, Paragraph, Sort, Tag } from "../../components";
+import {
+  Advantages,
+  Htag,
+  Paragraph,
+  Product,
+  Sort,
+  Tag,
+} from "../../components";
 import { TopPageComponentProps } from "./TopPageComponent.props";
 import styles from "./TopPageComponent.module.css";
 import { HhData } from "../../components/HhData/HhData";
@@ -34,7 +41,7 @@ export const TopPageComponent = ({
       </div>
       <div>
         {sortedProducts &&
-          sortedProducts.map((p) => <div key={p._id}>{p.title}</div>)}
+          sortedProducts.map((p) => <Product key={p._id} product={p} />)}
       </div>
       <div className={styles.hhTitle}>
         <Htag tag="h2">Вакансии - {page.category}</Htag>
