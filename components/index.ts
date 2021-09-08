78b8d7c55275324a8741340d1b3e05cbe51e00a0
voldo5 <45669506+voldo5@users.noreply.github.com>
2021-09-08 15:44:43 +0300

    006 Компонент продукта - стили

--- components/index.ts
+++ components/index.ts
@@ -9,6 +9,7 @@ export * from './Input/Input';
 export * from './Textarea/Textarea';
 export * from './Search/Search';
 export * from './Product/Product';
+export * from './Divider/Divider';
 
 // export * from './Htag/Htag';
 // export * from './Button/Button';
