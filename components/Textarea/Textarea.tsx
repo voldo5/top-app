341c0357154104eb6f2fdc6c71c8788b146ce045
voldo5 <45669506+voldo5@users.noreply.github.com>
2021-09-15 13:55:23 +0300

    005 Проброс ref

--- components/Textarea/Textarea.tsx
+++ components/Textarea/Textarea.tsx
@@ -1,9 +1,16 @@
-import { TextareaProps } from './Textarea.props';
-import styles from './Textarea.module.css';
-import cn from 'classnames';
+import { TextareaProps } from "./Textarea.props";
+import styles from "./Textarea.module.css";
+import cn from "classnames";
+import { ForwardedRef, forwardRef } from "react";
 
-export const Textarea = ({ className, ...props }: TextareaProps): JSX.Element => {
-	return (
-		<textarea className={cn(className, styles.input)} {...props} />
-	);
-};
+// eslint-disable-next-line react/display-name
+export const Textarea = forwardRef(
+  (
+    { className, ...props }: TextareaProps,
+    ref: ForwardedRef<HTMLTextAreaElement>
+  ): JSX.Element => {
+    return (
+      <textarea className={cn(className, styles.input)} ref={ref} {...props} />
+    );
+  }
+);
