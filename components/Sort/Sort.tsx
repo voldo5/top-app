e1717a5849b50a8ec818539790515d36dcfc5f60
voldo5 <45669506+voldo5@users.noreply.github.com>
2021-10-01 15:53:02 +0300

    Sort accessibility

--- components/Sort/Sort.tsx
+++ components/Sort/Sort.tsx
@@ -11,7 +11,7 @@ export const Sort = ({
 }: SortProps): JSX.Element => {
   return (
     <div className={cn(styles.sort, className)} {...props}>
-      <span
+      <button
         onClick={() => setSort(SortEnum.Rating)}
         className={cn({
           [styles.active]: sort == SortEnum.Rating,
@@ -19,8 +19,8 @@ export const Sort = ({
       >
         <SortIcon className={styles.sortIcon} />
         По рейтингу
-      </span>
-      <span
+      </button>
+      <button
         onClick={() => setSort(SortEnum.Price)}
         className={cn({
           [styles.active]: sort == SortEnum.Price,
@@ -28,7 +28,7 @@ export const Sort = ({
       >
         <SortIcon className={styles.sortIcon} />
         По цене
-      </span>
+      </button>
     </div>
   );
 };
